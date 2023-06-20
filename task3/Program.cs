ed9e3b249cdd406f1b913f1e2724f9348c1c3cf1
Igor_Potapoff <igor.potapoff@yandex.ru>
2023-06-20 23:14:17 +0300

    Task3

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+*/
+
+int RandomNumber (int min, int max)
+
+{
+int result = new Random().Next(min, max+1);
+
+return result;
+}
+
+void Print( string text)
+{
+Console.WriteLine(text);
+
+}
+
+void GetCube (int rand)
+{
+for (int i=1;i<=rand;i++)
+{
+
+Console.Write($"{Math.Pow(i,3)} ");
+
+}
+
+
+}
+
+int rand = RandomNumber(1,15);
+Print ($"{rand}");
+GetCube (rand);
+
+
+
+
