36d909b1cae6b9af3c8ad730a725266047dd4e21
Igor_Potapoff <igor.potapoff@yandex.ru>
2023-06-20 22:56:13 +0300

    Task1

---
+++ task1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+void Print(string text)
+{
+Console.WriteLine(text);
+}
+bool IsPalindrome (int number) // Исправил тут.
+{
+
+if ((number/10000 == number%10) && ((number/1000)%10 == (number%100)/10))
+    {
+    return true;
+    }
+    else
+    return false;
+    }
+Print("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // Исправил тут.
+
+if (number>=10000 && number<=99999)
+
+    if (IsPalindrome(number) ==true) // Исправил тут.
+    {
+    Console.WriteLine($"{number} - палиндром");
+    }
+    else
+    {
+    Console.WriteLine($"{number} - не палиндром");
+    }
+
+else
+
+Console.WriteLine($"{number} - не пятизначное число");
+
+
+
+
+
+
+
